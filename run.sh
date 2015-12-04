3657d7d821431fa50162b3dadb69fc2b95883ef5
John Krauss <irving.krauss@gmail.com>
2015-12-04 12:38:58 -0500

    PYTHONPATH to work inside docker

--- run.sh
+++ run.sh
@@ -6,6 +6,10 @@ if [ -f .env ]; then
   source .env
 fi
 
-PYTHONPATH=$PWD luigi --module tasks.us.census.acs AllACS --local-scheduler \
+export PYTHONPATH=$PWD:/bigmetadata
+luigi --module tasks.us.census.acs AllACS --local-scheduler \
    --parallel-scheduling --workers=8 \
    > logs/luigi.log 2>logs/luigi.err
+
+#luigi --module tasks.us.census.acs ProcessACS --force --year 2010 --sample 5yr --local-scheduler
+#luigi --module tasks.us.census.tiger Tiger --force --local-scheduler
