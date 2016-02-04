eacfb782cfbc15871a33d7da3b1d8da6138ce18b
John Krauss <irving.krauss@gmail.com>
2016-02-04 16:08:32 +0000

    dma to carto

--- tasks/us/dma.py
+++ tasks/us/dma.py
@@ -6,25 +6,65 @@ Bigmetadata tasks
 tasks to download and create metadata
 '''
 
-from luigi import Task, Parameter, LocalTarget
-
-
-class DownloadDMA(Task):
-
-    URL = 'https://andrew.cartodb.com/api/v2/sql?q=select+*+from+andrew.dma_master_polygons&format=csv'
-
-    def run(self):
-        pass
-
-    def output(self):
-        pass
+from luigi import Task, Parameter, LocalTarget, BooleanParameter
+from tasks.util import (DefaultPostgresTarget, classpath, pg_cursor, shell,
+                        CartoDBTarget, sql_to_cartodb_table)
 
 
 class DMA(Task):
 
-    def requires(self):
-        return DownloadDMA()
+    URL = 'https://andrew.cartodb.com/api/v2/sql?q=select+*+from+andrew.dma_master_polygons&format=csv'
+
+    def tablename(self):
+        return '"{schema}".dma'.format(schema=classpath(self))
+
+    def columns(self):
+        return '''
+"cartodb_id" int,
+"the_geom" geometry,
+"the_geom_webmercator" geometry,
+"dma_code" int,
+"dma_name" text
+    '''
 
     def run(self):
-        pass
+        cursor = pg_cursor()
+        cursor.execute('CREATE SCHEMA IF NOT EXISTS "{schema}"'.format(
+            schema=classpath(self)))
+        cursor.execute('''
+DROP TABLE IF EXISTS {tablename};
+CREATE TABLE {tablename} (
+    {columns}
+)
+                       '''.format(tablename=self.tablename(),
+                                  columns=self.columns()))
+        cursor.connection.commit()
+        shell('wget \'{url}\' -O - | '
+              r"psql -c '\copy {tablename} FROM STDIN WITH CSV HEADER'".format(
+                  url=self.URL,
+                  tablename=self.tablename()))
+        self.output().touch()
 
+    def output(self):
+        return DefaultPostgresTarget(table=self.tablename())
+
+
+class ExtractDMA(Task):
+
+    force = BooleanParameter(default=False)
+
+    def tablename(self):
+        return "us_dma_digital_marketing_areas"
+
+    def requires(self):
+        return DMA()
+
+    def run(self):
+        query = u'SELECT * FROM {table}'.format(table=self.input().table)
+        sql_to_cartodb_table(self.tablename(), query)
+
+    def output(self):
+        target = CartoDBTarget(self.tablename())
+        if self.force and target.exists():
+            target.remove()
+        return target
