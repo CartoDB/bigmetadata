87f4f5c27353824a9443ca508dcbb7e3347ddc01
Andy Eschbacher <andy.eschbacher@gmail.com>
2016-05-10 12:44:19 -0400

    adding more datasets to etl

--- tasks/us/zillow.py
+++ tasks/us/zillow.py
@@ -17,21 +17,55 @@ from tasks.us.census.tiger import GeoidColumns
 from psycopg2 import ProgrammingError
 
 
+class ExtractAllZillow(WrapperTask):
+    
+    def requires(self):            
+        ## go across all types
+        geographies = ('State', 'Metro', 'County', 'City', 'Zip', 'Neighborhood',)
+        
+        ## cherry-picked datasets
+        hometypes = ('AllHomes', 'SingleFamilyResidence',)
+        rentaltypes = ('AllHomesPlusMultifamily','SingleFamilyResidenceRental', 'Sfr','AllHomes',)
+        
+        ## Zillow Measures
+        home_measure = 'Zhvi'
+        rental_measure = 'Zri'
+        
+        ## Median Value per SqFt
+        hometypes_medians =   ('AllHomes',)
+        rentaltypes_medians = ('AllHomes', 'Sfr', 'CondoCoop',)
+        
+        ## Measures for amount per square foot
+        median_sqft_houses = 'MedianValuePerSqft'
+        median_sqft_rental = 'MedianRentalPricePerSqft'
+        
+        for g in geographies:
+            for h in hometypes:
+                yield DownloadZillow(geography=g, hometype=h, measure=home_measure)
+            for r in rentaltypes:
+                yield DownloadZillow(geography=g, hometype=h, measure=home_measure)
+            for hm in hometypes_medians:
+                yield DownloadZillow(geography=g, hometype=hm, measure=median_sqft_houses)
+            for rm in rentaltypes_medians:
+                yield DownloadZillow(geography=g, hometype=rm, measure=median_sqft_rental)
+            
+
 class DownloadZillow(Task):
     
-    geography = Parameter(default='Zip') # example: Zip
-    hometype = Parameter(default='SingleFamilyResidence') # example: SingleFamilyResidence
+    geography = Parameter(default='Zip')
+    hometype = Parameter(default='SingleFamilyResidence')
+    measure = Parameter(default='Zhvi')
     
-    URL = 'http://files.zillowstatic.com/research/public/{geography}/{geography}_Zhvi_{hometype}.csv'
+    
+    URL = 'http://files.zillowstatic.com/research/public/{geography}/{geography}_{measure}_{hometype}.csv'
     
     def run(self):
         self.output().makedirs()
         shell('wget \'{url}\' -O {output}'.format(
-            url=self.URL.format(geography=self.geography, hometype=self.hometype), output=self.output().path))
+            url=self.URL.format(geography=self.geography, hometype=self.hometype), measure=self.measure, output=self.output().path))
         
     def output(self):
         return LocalTarget(os.path.join('tmp', classpath(self), self.task_id) + '.csv')
-
 
 class ZillowColumns(ColumnsTask):
 
@@ -45,7 +79,7 @@ class ZillowColumns(ColumnsTask):
                                    weight=0)),
             ('RegionName', OBSColumn(type='Text',
                                      name='ZCTA5',
-                                     description="",
+                                     description="Zip Code Tabulation Area id",
                                      weight=0)),
             ('City', OBSColumn(type='Text',
                                name='City Name',
@@ -107,9 +141,12 @@ class Zillow(TableTask):
     def columns(self):
         if self.geography == 'Zip':
             tiger_geo = 'zcta5'
-        # elif self.geography == 'State':
-        #     tiger_geo = 'geoid'
+        elif self.geography == 'State':
+            tiger_geo = 'geoid'
+        elif self.geography == 'County':
+            tiger_geom = 'county'
         else:
+            ## will happen for metro areas, cities, neighborhoods
             raise Exception('unrecognized geography {}'.format(self.geography))
         
         columns = OrderedDict()
