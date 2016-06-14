5ed79b3ca213010397e9cff14bc2e6198e318bd0
John Krauss <irving.krauss@gmail.com>
2016-06-14 13:01:05 +0000

    use new convenience class for scotland

--- tasks/uk/scotlandcensus.py
+++ tasks/uk/scotlandcensus.py
@@ -2,21 +2,15 @@
 
 from luigi import Task, Parameter, LocalTarget
 
-from tasks.util import TableTask, ColumnsTask, classpath, shell
+from tasks.util import (TableTask, ColumnsTask, classpath, shell,
+                        DownloadUnzipTask)
 
 import os
 
-class DownloadScotlandLocal(Task):
+class DownloadScotlandLocal(DownloadUnzipTask):
 
     URL = 'http://www.scotlandscensus.gov.uk/ods-web/download/getDownloadFile.html?downloadFileIds=Output%20Area%20blk'
 
-    def run(self):
-        self.output().makedirs()
+    def download(self):
         shell('wget -O {output}.zip {url}'.format(output=self.output().path,
                                                   url=self.URL))
-        os.makedirs(self.output().path)
-        shell('unzip -d {output} {output}.zip'.format(output=self.output().path))
-
-    def output(self):
-        return LocalTarget(os.path.join('tmp', classpath(self)))
-
