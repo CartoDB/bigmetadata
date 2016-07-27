4411fdb3d0b8fade1d2ea23f1c06de4f34dae85d
John Krauss <irving.krauss@gmail.com>
2016-07-27 13:24:44 +0000

    ipython should be able to run luigi tasks

--- ipython/start.sh
+++ ipython/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-mkdir -p /bigmetadata/tmp/ipython
+sudo mkdir -p /bigmetadata/tmp/ipython
+sudo chown -R ds:ds /bigmetadata/tmp/ipython
 /opt/ds/bin/jupyter-notebook --no-browser --port 8888 --ip=0.0.0.0 --notebook-dir=/bigmetadata/tmp/ipython
