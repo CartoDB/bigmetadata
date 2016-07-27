69af6bdf2e7673ffc3bfb86202e1657c478e7533
John Krauss <irving.krauss@gmail.com>
2016-07-27 15:03:39 +0000

    improvements to test

--- tests/test_columntasks.py
+++ tests/test_columntasks.py
@@ -7,7 +7,7 @@ from tests.util import session_scope, runtask, setup, teardown
 from tasks.es.ine import FiveYearPopulationColumns
 from tasks.meta import (get_engine, current_session, Base, session_commit,
                         session_rollback, OBSColumn)
-from tasks.util import shell, ColumnsTask, classpath
+from tasks.util import shell, ColumnsTask, TableTask, classpath
 
 from nose import with_setup
 from nose.tools import assert_greater
@@ -19,7 +19,7 @@ import inspect
 import os
 
 
-def column_tasks():
+def collect_tasks(TaskClass):
     tasks = set()
     for dirpath, _, files in os.walk('tasks'):
         for filename in files:
@@ -30,18 +30,17 @@ def column_tasks():
                 ])
                 module = importlib.import_module(modulename)
                 for _, obj in inspect.getmembers(module):
-                    if inspect.isclass(obj) and issubclass(obj, ColumnsTask) and obj != ColumnsTask:
+                    if inspect.isclass(obj) and issubclass(obj, TaskClass) and obj != TaskClass:
                         tasks.add((obj, ))
     return tasks
 
 
 
 @with_setup(setup, teardown)
-@parameterized(column_tasks())
+@parameterized(collect_tasks(ColumnsTask))
 def test_column_task(klass):
     # Ensure every column task runs and produces some kind of independent
     # metadata.
-    print klass
     with session_scope() as session:
         # TODO: test columnstasks that have params
         if klass.get_param_names():
@@ -51,3 +50,25 @@ def test_column_task(klass):
         assert_greater(
             session.query(OBSColumn).filter(
                 OBSColumn.id.startswith(classpath(task))).count(), 0)
+
+
+#@with_setup(setup, teardown)
+#@parameterized(collect_tasks(TableTask))
+#def test_table_task(klass):
+#    # Ensure every column task runs and produces some kind of independent
+#    # metadata.
+#    with session_scope() as session:
+#        # TODO: test tabletasks that have params
+#        if klass.get_param_names():
+#            raise SkipTest('Cannot test TableTask with params')
+#        task = klass()
+#        # we don't do a full `run` as that's too expensive
+#        # instead, we run any column reqs and make sure the `columns` method
+#        # works after that
+#        for dep in task.deps():
+#            if issubclass(dep, ColumnsTask):
+#                runtask(dep)
+#        task.columns()
+#        #assert_greater(
+#        #    session.query(OBSColumn).filter(
+#        #        OBSColumn.id.startswith(classpath(task))).count(), 0)
