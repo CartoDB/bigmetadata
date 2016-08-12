87fc8e9821ba4a0598c3ecd31e678a77bdb30bde
John Krauss <irving.krauss@gmail.com>
2016-08-12 17:13:44 +0000

    proper relative path for import

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ import os
 import subprocess
 import sys
 
-sys.path.insert(0, os.path.abspath('../tasks'))
+sys.path.insert(0, os.path.abspath('../../tasks'))
 
 # -- General configuration ------------------------------------------------
 
