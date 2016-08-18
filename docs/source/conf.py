6288ceb9916606d5cd7649384572a007f33cbf45
John Krauss <irving.krauss@gmail.com>
2016-08-18 16:48:36 +0000

    fix path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -358,7 +358,7 @@ def run_apidoc(_):
     modules = ['../../tasks']
     for module in modules:
         cur_dir = os.path.abspath(os.path.dirname(__file__))
-        output_path = os.path.join(cur_dir, module, 'doc')
+        output_path = os.path.join(cur_dir, module, 'docs')
         cmd_path = 'sphinx-apidoc'
         if hasattr(sys, 'real_prefix'):  # Check to see if we are in a virtualenv
             # If we are, assemble the path manually
