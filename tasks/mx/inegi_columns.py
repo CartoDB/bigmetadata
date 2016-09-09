e2f8401b101d01be31436c1285952e3e0c7a3d7b
Michelle Ho <mho@CARTOs-Air.fios-router.home>
2016-09-09 11:27:31 -0400

    not sure what source should be for inegi_columns still

--- tasks/mx/inegi_columns.py
+++ tasks/mx/inegi_columns.py
@@ -1,10 +1,20 @@
 from tasks.meta import OBSColumn, DENOMINATOR
-from tasks.util import ColumnsTask
+from tasks.util import ColumnsTask, TagsTask
 from tasks.tags import SectionTags, SubsectionTags, UnitTags
 
 from collections import OrderedDict
 
 # %s/\(\w\+\d\+_\?R\?\)\t\(.\+\)$/\1 = OBSColumn(\r    id='\1',\r    name='\2',\r    type='Numeric',\r    weight=3,\r    aggregate='sum',\r    tags=[mexico, unit_people, ],\r    targets={},\r)
+
+class INEIGITags(TagsTask)
+    def version(self):
+        return 1
+
+    def tags(self):
+        return [
+            OBSTag(id='ineigi',
+                name=''
+                )]
 
 class DemographicColumns(ColumnsTask):
 
