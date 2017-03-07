38873bb954c48d7f679441ad8ebf57eec4cfbac2
Michelle Ho <michellemmh@gmail.com>
2017-03-07 10:43:33 -0500

    adding partial catalog test

---
+++ tests/test_catalog.py
@@ -0,0 +1,20 @@
+from tests.util import runtask, setup, teardown, collect_meta_wrappers
+
+from tasks.carto import OBSMetaToLocal
+from tasks.meta import current_session
+from tasks.util import MetaWrapper, TagsTask, ColumnsTask, TableTask
+from tasks.sphinx import Catalog
+
+from nose_parameterized import parameterized
+from nose.tools import assert_greater, with_setup
+
+
+@with_setup(setup, teardown)
+def test_catalog():
+
+    for klass, params in collect_meta_wrappers():
+        task = klass(**params)
+        runtask(task, superclasses=[TagsTask, ColumnsTask, TableTask])
+
+    runtask(OBSMetaToLocal())
+    runtask(Catalog(force=True))
