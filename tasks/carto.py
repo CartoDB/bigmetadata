0ae95bd3f0269b3ad7e2bc493369da91e1def1c6
Javier Torres <jtorres@carto.com>
2017-08-10 12:40:17 +0200

    Add new meta tables to OBSMetaToLocal result

--- tasks/carto.py
+++ tasks/carto.py
@@ -1422,7 +1422,7 @@ class OBSMetaToLocal(OBSMeta):
 
     def output(self):
         tables = ['obs_meta', 'obs_meta_numer', 'obs_meta_denom',
-                  'obs_meta_geom', 'obs_meta_timespan']
+                  'obs_meta_geom', 'obs_meta_timespan', 'obs_meta_geom_numer_timespan']
 
         return [PostgresTarget('observatory', t, non_empty=False) for t in tables] + [UpdatedMetaTarget()]
 
