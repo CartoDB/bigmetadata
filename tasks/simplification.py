099a9f9f6dee40db5ca8f7bf4b5cec85323e23ad
Antonio <acarlon@cartodb.com>
2017-09-22 12:49:10 +0200

    Added PostGIS simplification (VW)

--- tasks/simplification.py
+++ tasks/simplification.py
@@ -1,13 +1,17 @@
 import os
 from luigi import Task, WrapperTask, Parameter, LocalTarget
 from util import PostgresTarget, shell
+from meta import CurrentSession
 
+DEFAULT_GEOMFIELD = 'the_geom'
 TMP_DIRECTORY = 'tmp'
 SIMPLIFICATION_DIRECTORY = 'simplification'
 SIMPLIFIED_SUFFIX = '_simpl'
-RETAIN_PERCENTAGE = '50'  # [0-100] Percentage of removable vertices retained
 SKIPFAILURES_NO = 'no'  # Avoids https://trac.osgeo.org/gdal/ticket/6803
 SKIPFAILURES_YES = 'yes'
+DEFAULT_M_RETAIN_PERCENTAGE = '50'  # [0-100] Percentage of removable vertices retained by mapshaper
+DEFAULT_P_RETAIN_FACTOR = '50'  # Retain factor used for PostGIS simplification (this is NOT a percentage)
+                                # The higher the retain factor, the lower the simplification
 
 
 def tmp_directory(schema, table):
@@ -40,7 +44,7 @@ class ExportShapefile(Task):
 class SimplifyShapefile(Task):
     schema = Parameter()
     table = Parameter()
-    factor = Parameter(default=RETAIN_PERCENTAGE)
+    retainpercentage = Parameter(default=DEFAULT_M_RETAIN_PERCENTAGE)
     skipfailures = Parameter(default=SKIPFAILURES_NO)
 
     def requires(self):
@@ -48,10 +52,10 @@ class SimplifyShapefile(Task):
 
     def run(self):
         cmd = 'node --max-old-space-size=8192 `which mapshaper` ' \
-              '{input} snap -simplify {factor}% keep-shapes -o {output}'.format(
+              '{input} snap -simplify {retainpercentage}% keep-shapes -o {output}'.format(
                 input=os.path.join(tmp_directory(self.schema, self.table),
                                    shp_filename(self.schema, self.table)),
-                factor=self.factor,
+                retainpercentage=self.retainpercentage,
                 output=self.output().path)
         shell(cmd)
 
@@ -63,31 +67,36 @@ class SimplifyShapefile(Task):
 class ImportSimplifiedShapefile(Task):
     schema = Parameter()
     table = Parameter()
-    outsuffix = Parameter(default='')
-    factor = Parameter(default=RETAIN_PERCENTAGE)
+    retainpercentage = Parameter(default=DEFAULT_M_RETAIN_PERCENTAGE)
     skipfailures = Parameter(default=SKIPFAILURES_NO)
 
     executed = False
 
     def requires(self):
-        yield SimplifyShapefile(schema=self.schema, table=self.table, factor=self.factor,
+        yield SimplifyShapefile(schema=self.schema, table=self.table, retainpercentage=self.retainpercentage,
                                 skipfailures=self.skipfailures)
 
     def run(self):
+        session = CurrentSession().get()
+
+        session.execute('TRUNCATE TABLE "{schema}".{table} '.format(
+                         schema=self.output().schema, table=self.output().tablename))
+        session.execute("COMMIT")
+
         cmd = 'PG_USE_COPY=yes ' \
-              'ogr2ogr -f PostgreSQL "PG:dbname=$PGDATABASE active_schema={schema}" -overwrite ' \
+              'ogr2ogr -f PostgreSQL "PG:dbname=$PGDATABASE active_schema={schema}" ' \
               '-t_srs "EPSG:4326" -nlt MultiPolygon -nln {table} ' \
-              '-lco OVERWRITE=yes -lco PRECISION=no ' \
+              '-lco OVERWRITE=yes -lco PRECISION=no -lco GEOMETRY_NAME= ' \
               '-lco SCHEMA={schema} {shp_path} '.format(
-                    table=self.output().tablename,
                     schema=self.output().schema,
+                    table=self.output().tablename,
                     shp_path=os.path.join(tmp_directory(self.schema, self.table),
                                           shp_filename(self.schema, self.table, SIMPLIFIED_SUFFIX)))
         shell(cmd)
         self.executed = True
 
     def output(self):
-        return PostgresTarget(self.schema, self.table + self.outsuffix)
+        return PostgresTarget(self.schema, self.table)
 
     def complete(self):
         if not self.executed:
@@ -95,13 +104,52 @@ class ImportSimplifiedShapefile(Task):
         return self.output().exists()
 
 
-class SimplifyGeometries(WrapperTask):
+class SimplifyGeometriesMapshaper(WrapperTask):
     schema = Parameter()
     table = Parameter()
-    outsuffix = Parameter(default='')
-    factor = Parameter(default=RETAIN_PERCENTAGE)
+    retainpercentage = Parameter(default=DEFAULT_M_RETAIN_PERCENTAGE)
     skipfailures = Parameter(default=SKIPFAILURES_NO)
 
     def requires(self):
-        yield ImportSimplifiedShapefile(schema=self.schema, table=self.table, outsuffix=self.outsuffix,
-                                        factor=self.factor, skipfailures=self.skipfailures)
+        yield ImportSimplifiedShapefile(schema=self.schema, table=self.table, retainpercentage=self.retainpercentage,
+                                        skipfailures=self.skipfailures)
+
+
+def postgis_simplification_factor(schema, table, geomfield, divisor_power):
+    session = CurrentSession().get()
+    return session.execute('SELECT AVG(ST_Perimeter({geomfield}) / ST_NPoints({geomfield})) / 10 ^ ({divisor} / 10) '
+                           'from "{schema}".{table}'.format(
+                            schema=schema, table=table, geomfield=geomfield, divisor=divisor_power
+                           )).fetchone()[0]
+
+
+class SimplifyGeometriesPostGIS(Task):
+    schema = Parameter()
+    table = Parameter()
+    geomfield = Parameter(default=DEFAULT_GEOMFIELD)
+    retainfactor = Parameter(default='?')
+
+    executed = False
+
+    def run(self):
+        session = CurrentSession().get()
+
+        if self.retainfactor == '?':
+            self.retainfactor = DEFAULT_P_RETAIN_FACTOR
+
+        factor = postgis_simplification_factor(self.schema, self.table, self.geomfield, self.retainfactor)
+
+        session.execute('UPDATE "{schema}".{table} '
+                        'SET {geomfield} = ST_MakeValid(ST_SimplifyVW({geomfield}, {factor})); '.format(
+                            schema=self.schema, table=self.table, geomfield=self.geomfield, factor=factor))
+        session.execute("COMMIT")
+
+        self.executed = True
+
+    def output(self):
+        return PostgresTarget(self.schema, self.table)
+
+    def complete(self):
+        if not self.executed:
+            return False
+        return self.output().exists()
