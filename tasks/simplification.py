ea1670888b85b41fe46a93c3c1f9543c79201c66
Antonio <acarlon@cartodb.com>
2017-09-25 11:19:43 +0200

    Simplification for observatory schema tables

--- tasks/simplification.py
+++ tasks/simplification.py
@@ -3,6 +3,7 @@ from luigi import Task, WrapperTask, Parameter, LocalTarget
 from util import PostgresTarget, shell
 from meta import CurrentSession
 
+OBSERVATORY_SCHEMA = 'observatory'
 DEFAULT_GEOMFIELD = 'the_geom'
 TMP_DIRECTORY = 'tmp'
 SIMPLIFICATION_DIRECTORY = 'simplification'
@@ -10,8 +11,9 @@ SIMPLIFIED_SUFFIX = '_simpl'
 SKIPFAILURES_NO = 'no'  # Avoids https://trac.osgeo.org/gdal/ticket/6803
 SKIPFAILURES_YES = 'yes'
 DEFAULT_M_RETAIN_PERCENTAGE = '50'  # [0-100] Percentage of removable vertices retained by mapshaper
-DEFAULT_P_RETAIN_FACTOR = '50'  # Retain factor used for PostGIS simplification (this is NOT a percentage)
-                                # The higher the retain factor, the lower the simplification
+DEFAULT_P_RETAIN_FACTOR = '50'  # Retain factor used for PostGIS simplification (this is NOT a percentage) \
+# The higher the retain factor, the lower the simplification
+UNKNOWN_RETAIN_FACTOR = '?'
 
 
 def tmp_directory(schema, table):
@@ -127,7 +129,7 @@ class SimplifyGeometriesPostGIS(Task):
     schema = Parameter()
     table = Parameter()
     geomfield = Parameter(default=DEFAULT_GEOMFIELD)
-    retainfactor = Parameter(default='?')
+    retainfactor = Parameter(default=UNKNOWN_RETAIN_FACTOR)
 
     executed = False
 
@@ -153,3 +155,73 @@ class SimplifyGeometriesPostGIS(Task):
         if not self.executed:
             return False
         return self.output().exists()
+
+
+class MapshaperSimplification():
+    def __init__(self, retainpercentage=DEFAULT_M_RETAIN_PERCENTAGE, skipfailures=SKIPFAILURES_NO):
+        self.retainpercentage = retainpercentage
+        self.skipfailures = skipfailures
+
+
+class PostGISSimplification():
+    def __init__(self, geomfield=DEFAULT_GEOMFIELD, retainfactor=UNKNOWN_RETAIN_FACTOR):
+        self.geomfield = geomfield
+        self.retainfactor = retainfactor
+
+
+class Simplify(Task):
+    schema = Parameter()
+
+    dont_simplify = None
+    '''
+    Array of names of tables in the schema that we don't want to simplify
+    Example: dont_simplify = ['mytable1', 'mytable2']
+    '''
+
+    override_defaults = None
+    '''
+    Array of Dict objects to override default simplification
+    Example: [('mytable', PostGISSimplification(retainfactor=60))]
+    (simplify 'mytable' using a PostGIS simplification with a retain factor of 60)
+    '''
+
+    executed = False
+
+    def requires(self):
+        for _tableid, _tablename in self.find_tables(self.schema):
+            yield self.find_simplification(_tableid, _tablename)
+
+    def run(self):
+        self.executed = True
+
+    def complete(self):
+        return self.executed
+
+    def find_tables(self, schema):
+        session = CurrentSession().get()
+
+        return session.execute("SELECT id, tablename FROM observatory.obs_table "
+                               "WHERE the_geom IS NOT NULL AND id LIKE '{schema}%' "
+                               "{dont_simplify} ".format(
+                                    schema=schema,
+                                    dont_simplify="" if self.dont_simplify is None
+                                                  else "AND id NOT SIMILAR TO '" + self.schema + ".("
+                                                  + "|".join([x for x in self.dont_simplify]) + ")%'"
+                                )).fetchall()
+
+    def find_simplification(self, tableid, tablename):
+        simplification = SimplifyGeometriesMapshaper(OBSERVATORY_SCHEMA, tablename)
+
+        if self.override_defaults is not None:
+            for _table, _simplification in self.override_defaults:
+                if tableid.startswith('{schema}.{table}'.format(schema=self.schema, table=_table)):
+                    if isinstance(_simplification, MapshaperSimplification):
+                        simplification = SimplifyGeometriesMapshaper(OBSERVATORY_SCHEMA, tablename,
+                                                                     _simplification.retainpercentage,
+                                                                     _simplification.skipfailures)
+                    elif isinstance(_simplification, PostGISSimplification):
+                        simplification = SimplifyGeometriesPostGIS(OBSERVATORY_SCHEMA, tablename,
+                                                                   _simplification.geomfield,
+                                                                   _simplification.retainfactor)
+
+        return simplification
