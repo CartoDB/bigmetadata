7ebc2fbd4eceeeed092ae9e97a771e720c78c28e
Antonio <acarlon@cartodb.com>
2017-10-25 13:14:20 +0200

    Added CR suggestions

--- tasks/simplify.py
+++ tasks/simplify.py
@@ -3,7 +3,7 @@ import json
 import os
 from util import get_logger
 from simplification import SimplifyGeometriesMapshaper, SimplifyGeometriesPostGIS
-
+from meta import CurrentSession
 
 TABLE_ID = 'tableid'
 SIMPLIFICATION = 'simplification'
@@ -19,6 +19,12 @@ SIMPLIFICATION_POSTGIS = 'postgis'
 LOGGER = get_logger(__name__)
 
 
+def find_table_name(table_id):
+    session = CurrentSession().get()
+    return session.execute("SELECT tablename FROM observatory.obs_table WHERE id = '{tableid}'".format(
+                            tableid=table_id)).fetchone()[0]
+
+
 class SimplifyOne(Task):
     table = Parameter()
 
@@ -26,30 +32,24 @@ class SimplifyOne(Task):
         with open(os.path.join(os.path.dirname(__file__), 'simplifications.json')) as file:
             tables = json.load(file)
 
-            if self.table in tables:
-                table = tables.get(self.table)
-                LOGGER.info("Simplifying %s (%s) using %s", self.table, table.get(TABLE_ID), table.get(SIMPLIFICATION))
+            table = tables[self.table]
+            tablename = find_table_name(self.table)
+            LOGGER.info("Simplifying %s (%s) using %s", self.table, tablename, table.get(SIMPLIFICATION))
 
-                simplification = table.get(SIMPLIFICATION)
-                if simplification == SIMPLIFICATION_MAPSHAPER:
-                    yield SimplifyGeometriesMapshaper(schema=OBSERVATORY_SCHEMA, table_input=self.table,
-                                                      geomfield=table[GEOM_FIELD], retainfactor=table[FACTOR],
-                                                      skipfailures=table[SKIP_FAILURES], maxmemory=table[MAX_MEMORY])
-                elif simplification == SIMPLIFICATION_POSTGIS:
-                    yield SimplifyGeometriesPostGIS(schema=OBSERVATORY_SCHEMA, table_input=self.table,
-                                                    geomfield=table[GEOM_FIELD], retainfactor=table[FACTOR])
-                else:
-                    raise ValueError('Invalid simplification "{simplification}" for {table}'.format(
-                        simplification=simplification, table=self.table))
+            simplification = table[SIMPLIFICATION]
+            if simplification == SIMPLIFICATION_MAPSHAPER:
+                yield SimplifyGeometriesMapshaper(schema=OBSERVATORY_SCHEMA, table_input=tablename,
+                                                  geomfield=table[GEOM_FIELD], retainfactor=table[FACTOR],
+                                                  skipfailures=table[SKIP_FAILURES], maxmemory=table[MAX_MEMORY])
+            elif simplification == SIMPLIFICATION_POSTGIS:
+                yield SimplifyGeometriesPostGIS(schema=OBSERVATORY_SCHEMA, table_input=tablename,
+                                                geomfield=table[GEOM_FIELD], retainfactor=table[FACTOR])
+            else:
+                raise ValueError('Invalid simplification "{simplification}" for {table}'.format(
+                    simplification=simplification, table=self.table))
 
 
 class SimplifyAll(Task):
     def requires(self):
         with open(os.path.join(os.path.dirname(__file__), 'simplifications.json')) as file:
-            tables = json.load(file)
-
-            tasks = []
-            for table in tables:
-                tasks.append(SimplifyOne(table=table))
-
-            return tasks
+            return [SimplifyOne(table=table) for table in json.load(file)]
