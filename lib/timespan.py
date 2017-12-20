1ea817e21470da5876846f59222ee1bd8008a2dd
Antonio <acarlon@cartodb.com>
2017-12-20 16:16:27 +0100

    Solved primary key problems

--- lib/timespan.py
+++ lib/timespan.py
@@ -1,6 +1,6 @@
 import os
 import json
-from tasks.meta import OBSTimespan
+from tasks.meta import OBSTimespan, current_session
 
 TIMESPANS_FILE = 'timespans.json'
 TIMESPAN_ID = 'id'
@@ -14,7 +14,8 @@ def get_timespan(timespan_id):
     with open(JSONFile) as infile:
         timespan_json = json.load(infile).get(str(timespan_id))
 
-        return OBSTimespan(id=timespan_json.get(TIMESPAN_ID),
-                           name=timespan_json.get(TIMESPAN_NAME),
-                           description=timespan_json.get(TIMESPAN_DESCRIPTION),
-                           timespan=timespan_json.get(TIMESPAN_TIMESPAN))
+        timespan = OBSTimespan(id=timespan_json.get(TIMESPAN_ID),
+                               name=timespan_json.get(TIMESPAN_NAME),
+                               description=timespan_json.get(TIMESPAN_DESCRIPTION),
+                               timespan=timespan_json.get(TIMESPAN_TIMESPAN))
+        return current_session().merge(timespan)
