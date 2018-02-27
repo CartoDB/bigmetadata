22cd979f34eb3647284b232de963288cc78ebd93
Mario de Frutos <ethervoid@gmail.com>
2018-02-27 14:41:50 +0100

    Watcher checks and shows the result of the rebuild

--- scripts/watch_containers.py
+++ scripts/watch_containers.py
@@ -10,3 +10,8 @@ while(True):
     else:
         print("Still running containers. Sleeping...")
         time.sleep(60)
+
+success_finished_containers = client.containers.list(filters = {'status': 'exited', 'exited': '0', 'name': 'bigmetadata_bigmetadata_run'})
+finished_containers = client.containers.list(filters = {'status': 'exited', 'name': 'bigmetadata_bigmetadata_run'})
+failed_containers = [container for container in finished_containers if container not in set(success_finished_containers)]
+print("Finished. Total {} -- Successful {} -- Failed {}".format(len(finished_containers), len(success_finished_containers), len(failed_containers)))
