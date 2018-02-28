867fdaf4800b386da1a6aaeed474de3f5bd2b280
Mario de Frutos <ethervoid@gmail.com>
2018-02-28 19:14:53 +0100

    :lipstick

--- scripts/watch_containers.py
+++ scripts/watch_containers.py
@@ -3,6 +3,7 @@ import time
 import argparse
 from datetime import datetime
 
+
 def watch_containers(name, since=datetime.now(), pooling_time=60):
     api_client = docker.APIClient(base_url='unix://var/run/docker.sock')
     client = docker.from_env()
@@ -26,17 +27,20 @@ def watch_containers(name, since=datetime.now(), pooling_time=60):
             successful_containers.append(container)
     send_notification("Finished. Total {} -- Successful {} -- Failed {}".format(len(containers), len(successful_containers), len(failed_containers)))
 
+
 def get_active_containers(client, api_client, name, since):
     active_containers = []
-    for container in client.containers.list(filters = {'status': 'running', 'name': name}):
+    for container in client.containers.list(filters={'status': 'running', 'name': name}):
         container_started_at = api_client.inspect_container(container.id)['State']['StartedAt'].split('.')[0]
         starting_date = datetime.strptime(container_started_at, '%Y-%m-%dT%H:%M:%S')
         if starting_date >= since:
             active_containers.append(container)
     return active_containers
 
+
 def send_notification(message):
     print(message)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser('python3 watch_containers.py')
