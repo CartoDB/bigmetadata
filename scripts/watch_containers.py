57e5088e062354e1f603f7e5efa2e2da7ec853e9
Mario de Frutos <ethervoid@gmail.com>
2018-03-01 11:43:16 +0100

    Added notification option and show how many time it tooks to finish

--- scripts/watch_containers.py
+++ scripts/watch_containers.py
@@ -2,9 +2,10 @@ import docker
 import time
 import argparse
 from datetime import datetime
+from datetime import timedelta
 
 
-def watch_containers(name, since=datetime.now(), pooling_time=60):
+def watch_containers(name, since=datetime.now(), pooling_time=60, notification_channel='stdout'):
     api_client = docker.APIClient(base_url='unix://var/run/docker.sock')
     client = docker.from_env()
     containers = get_active_containers(client, api_client, name, since)
@@ -25,7 +26,10 @@ def watch_containers(name, since=datetime.now(), pooling_time=60):
             failed_containers.append(container)
         else:
             successful_containers.append(container)
-    send_notification("Finished. Total {} -- Successful {} -- Failed {}".format(len(containers), len(successful_containers), len(failed_containers)))
+    finish_time = datetime.utcnow() - since
+    finish_time_in_minutes = finish_time / timedelta(minutes=1)
+    message = "Finished. Total {} -- Successful {} -- Failed {}. It tooks {} minutes".format(len(containers), len(successful_containers), len(failed_containers), finish_time_in_minutes)
+    send_notification(notification_channel, message)
 
 
 def get_active_containers(client, api_client, name, since):
@@ -38,8 +42,15 @@ def get_active_containers(client, api_client, name, since):
     return active_containers
 
 
-def send_notification(message):
-    print(message)
+def send_notification(channel, message):
+    if channel == 'stdout':
+        print(message)
+    elif channel == 'slack':
+        raise NotImplementedError('Slack notification is not implemented yet')
+    elif channel == 'logfile':
+        raise NotImplementedError('Log file notification is not implemented yet')
+    else:
+        raise ('Notification channel option unknown')
 
 
 if __name__ == "__main__":
@@ -47,5 +58,6 @@ if __name__ == "__main__":
     parser.add_argument('name', help='Name, complete or just a part, of the container to check')
     parser.add_argument('--since', type=lambda s: datetime.strptime(s, '%Y-%m-%d %H:%M:%S'), help='Date where the containers where started. Supported format YYYY-mm-dd HH:MM:SS')
     parser.add_argument('--pooling-time', default=60, type=int, help='Time, in seconds, to check for the state of the active containers')
+    parser.add_argument('--notification-channel', default='stdout', choices=['stdout', 'slack', 'logfile'], help='Notification channel')
     parsed_args = vars(parser.parse_args())
     watch_containers(**parsed_args)
