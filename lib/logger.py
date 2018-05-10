73b8e8634ab4a660a856ee1c0d5a6b21fb448473
Mario de Frutos <ethervoid@gmail.com>
2018-05-10 12:42:26 +0200

    Logger should be the one defined in the config

--- lib/logger.py
+++ lib/logger.py
@@ -7,9 +7,4 @@ def get_logger(name):
     Obtain a logger outputing to stderr with specified name. Defaults to INFO
     log level.
     '''
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
-    handler = logging.StreamHandler(sys.stdout)
-    handler.setFormatter(logging.Formatter('%(asctime)-15s %(message)s'))
-    logger.addHandler(handler)
-    return logger
+    return logging.getLogger(name)
