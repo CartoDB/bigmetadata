366f95134273ee1fccdd5ab7b23f1cbfd07d5ab7
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2019-02-14 14:12:23 +0100

    removeFromDO accepts exceptions

--- tasks/tools.py
+++ tasks/tools.py
@@ -8,9 +8,11 @@ EXTRACT_NULLS_FROM_LOG = "nullsfromlog"
 MERGE_NULLS_FILES = "mergenullsfiles"
 
 
-def remove_from_do(id):
+def remove_from_do(id, exclusions_string = None):
     MINIMUM_ID_LENGTH = 5
     MAX_COLS_TO_PRINT = 50
+
+    exclusions = exclusions_string.split(',') if exclusions_string else []
 
     if len(id) < MINIMUM_ID_LENGTH:
         print("The identifier '{}' is too short (minimum {} characters)".format(id, MINIMUM_ID_LENGTH))
@@ -18,24 +20,29 @@ def remove_from_do(id):
 
     session = CurrentSession().get()
 
+    id_query = "id LIKE '{}%'".format(id)
+    if exclusions:
+        exclusions_quoted = ','.join(["'{}%'".format(e) for e in exclusions])
+        id_query += ' AND id NOT LIKE ALL(ARRAY[{}])'.format(exclusions_quoted)
+
     numtables = session.execute(
-        "SELECT count(*) FROM observatory.obs_table WHERE id LIKE '{}%'"
-        .format(id)).fetchone()[0]
+        "SELECT count(*) FROM observatory.obs_table WHERE {}"
+        .format(id_query)).fetchone()[0]
     print(" --> This will delete {} entries from observatory.OBS_TABLE".format(numtables))
-    for table in session.execute("SELECT id FROM observatory.obs_table WHERE id LIKE '{}%'".format(id)).fetchall():
+    for table in session.execute("SELECT id FROM observatory.obs_table WHERE {}".format(id_query)).fetchall():
         print("\t" + table[0])
 
     numcols = session.execute(
-        "SELECT count(*) FROM observatory.obs_column WHERE id LIKE '{}%'"
-        .format(id)).fetchone()[0]
+        "SELECT count(*) FROM observatory.obs_column WHERE {}"
+        .format(id_query)).fetchone()[0]
     print(" --> This will delete {} entries from observatory.OBS_COLUMNS".format(numcols))
     for column in session.execute(
-            "SELECT id FROM observatory.obs_column WHERE id LIKE '{}%'".format(id)).fetchmany(MAX_COLS_TO_PRINT):
+            "SELECT id FROM observatory.obs_column WHERE {}".format(id_query)).fetchmany(MAX_COLS_TO_PRINT):
         print("\t" + column[0])
     if numcols > MAX_COLS_TO_PRINT:
         print("\t... ({} more)".format(numcols - MAX_COLS_TO_PRINT))
     print(" --> This will drop {} tables from the 'observatory' schema".format(session.execute(
-        "SELECT count(*) FROM observatory.obs_table WHERE id LIKE '{}%'".format(id)).fetchone()[0]))
+        "SELECT count(*) FROM observatory.obs_table WHERE {}".format(id_query)).fetchone()[0]))
 
     yn = input("Continue? (Y/N) ")
 
@@ -43,12 +50,12 @@ def remove_from_do(id):
         return
 
     for table in session.execute(
-            "SELECT tablename FROM observatory.obs_table WHERE id LIKE '{}%'".format(id)).fetchall():
+            "SELECT tablename FROM observatory.obs_table WHERE {}".format(id_query)).fetchall():
         session.execute("DROP TABLE observatory.{}".format(table[0]))
         print("Table {} dropped".format(table[0]))
-    session.execute("DELETE FROM observatory.obs_table WHERE id LIKE '{}%'".format(id))
+    session.execute("DELETE FROM observatory.obs_table WHERE {}".format(id_query))
     print("Deleted {} entries from observatory.OBS_TABLE".format(numtables))
-    session.execute("DELETE FROM observatory.obs_column WHERE id LIKE '{}%'".format(id))
+    session.execute("DELETE FROM observatory.obs_column WHERE {}".format(id_query))
     print("Deleted {} entries from observatory.OBS_COLUMNS".format(numcols))
 
     session.execute("COMMIT")
@@ -124,16 +131,22 @@ if __name__ == "__main__":
     parser.add_argument('task', help='Task to be executed')
     parser.add_argument('task_parameters', nargs='+', help='Task parameters')
     args = vars(parser.parse_args())
-    if args['task'].lower() == REMOVE_FROM_DO and len(args['task_parameters']) == 1:
-        remove_from_do(args['task_parameters'][0])
+    task_params = args['task_parameters']
+    if args['task'].lower() == REMOVE_FROM_DO:
+        if len(task_params) == 1:
+            remove_from_do(task_params[0])
+        elif len(task_params) == 2:
+            remove_from_do(task_params[0], task_params[1])
+        else:
+            print('usage: python tools.py removeFromDO id [optional comma-separated list of exclusions]')
     elif args['task'].lower() == EXTRACT_NULLS_FROM_LOG:
-        if len(args['task_parameters']) == 3:
-            extract_nulls_from_log(args['task_parameters'][0], args['task_parameters'][1], args['task_parameters'][2])
+        if len(task_params) == 3:
+            extract_nulls_from_log(task_params[0], task_params[1], task_params[2])
         else:
             print('usage: python tools.py nullsfromlog file_in file_out relevant,fields,comma,separated')
     elif args['task'].lower() == MERGE_NULLS_FILES:
-        if len(args['task_parameters']) == 3:
-            merge_nulls_files(args['task_parameters'][0], args['task_parameters'][1], args['task_parameters'][2])
+        if len(task_params) == 3:
+            merge_nulls_files(task_params[0], task_params[1], task_params[2])
         else:
             print('usage: python tools.py mergenullsfiles file_in1 file_in2 file_out')
     else:
